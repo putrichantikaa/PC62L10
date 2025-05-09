ae21be320c4aede1428bf8c08ab1dfaa569f9a99
unknown <putrichantiikaa@gmail.com>
2025-05-09 10:31:11 +0700

    controller dosen

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,9 +30,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/mahasiswa">Data Mahasiswa</a>
+                        </li>
 
+                        <li class="nav-item">
+                            <a class="nav-link" href="/dosen">Data Dosen</a>
+                        </li>
                     </ul>
-
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
