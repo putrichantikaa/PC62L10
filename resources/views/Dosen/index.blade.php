1b6c1ded656505cb74a939fb1bee1a881a58a611
unknown <putrichantiikaa@gmail.com>
2025-05-16 08:44:11 +0700

    membuat form dosen

--- resources/views/Dosen/index.blade.php
+++ resources/views/Dosen/index.blade.php
@@ -2,13 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row mt-5">
-         <div class="col-12 m-auto">
-             <div class="card">
-             <div class="card-header">
-                 <h3 class="float-start">Data Dosen</h3>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Data Dosen') }}
                  <a href="dosen/form"><button type="button" class="btn btn-primary float-end">Tambah Data</button></a>
-             </div>
+                </div>
                  <div class="card-body">
                  <table class="table">
                     <thead>
@@ -22,9 +21,20 @@
                         <th scope="col">Action</th>
                       </tr>
                     </thead>
+                    <tbody>
+                        <tr>
+                          <th scope="row">1</th>
+                          <td>12121</td>
+                          <td>Putri Chantika</td>
+                          <td>Full Time</td>
+                          <td>putri@gmail.com</td>
+                          <td>081234654</td>
+                        </tr>
+                      </tbody>
                   </table>
             </div>
         </div>
     </div>
+</div>
 </div>
 @endsection
